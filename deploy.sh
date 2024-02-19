ff7d059145f342b7a1d8be00d65e29eaf62a59dc
Ryan Sepassi <rsepassi@gmail.com>
2024-02-18 16:41:05 -0800

    deploy script added

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+set -e
+
+./build.sh
+
+rm -f built.zip
+zip -r built.zip built
+
+curl -H "Content-Type: application/zip" \
+     -H "Authorization: Bearer $AUTH" \
+     --data-binary "@built.zip" \
+     https://api.netlify.com/api/v1/sites/quietcleanalliance.netlify.app/deploys
+
+rm -f built.zip
